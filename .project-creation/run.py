7c65c7e5a13b56ddc7309b8942913354cdb5942d
Markus Petke <MP91@users.noreply.github.com>
2023-09-08 14:01:54 +0200

    wip

---
+++ .project-creation/run.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2023 Robert Bosch GmbH
+
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import shutil
+import os
+
+
+def copy_files(destination: str):
+    for file in os.listdir(".."):
+        shutil.copy2(file, destination)
+
+
+def main():
+    parser = argparse.ArgumentParser("run")
+    parser.add_argument(
+        "-d",
+        "--destination",
+        type=str,
+        required=True,
+        help="Path to the root of the repository.",
+    )
+    parser.add_argument(
+        "-e",
+        "--example",
+        type=str,
+        required=False,
+        help="Copy the given example to the new repo.",
+    )
+    args = parser.parse_args()
+    copy_files(args.destination)
+
+
+if __name__ == "__main__":
+    main()
